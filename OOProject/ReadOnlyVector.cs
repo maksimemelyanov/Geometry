909e42ffc0e0e37452ede6c16c9465b50024e940
Nick <nikitaavdeev2000@gmail.com>
2019-12-06 16:58:42 +0500

    Расширил функциональность классов Vector и Segment, добавлен класс Geometry

--- OOProject/ReadOnlyVector.cs
+++ OOProject/ReadOnlyVector.cs
@@ -1,29 +1,89 @@
-﻿namespace ReadOnlyVectorTask
+﻿using System;
+
+namespace ReadOnlyVectorTask
 {
-    public class ReadOnlyVector
+    public class Segment
+    {
+        public readonly Vector Begin;
+        public readonly Vector End;
+
+        public Segment(Vector begin, Vector end)
+        {
+            Begin = begin;
+            End = end;
+        }
+
+        public Segment(double beginX, double beginY, double endX, double endY)
+        {
+            Begin = new Vector(beginX, beginY);
+            End = new Vector(endX, endY);
+        }
+
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public bool Contains(Vector vector)
+        {
+            return Geometry.IsVectorInSegment(vector, this);
+        }
+    }
+
+    public class Vector
     {
         public readonly double X;
         public readonly double Y;
 
-        public ReadOnlyVector(double x, double y)
+        public Vector(double x, double y)
         {
             X = x;
             Y = y;
         }
 
-        public ReadOnlyVector Add(ReadOnlyVector other)
+        public Vector Add(Vector other)
         {
-            return new ReadOnlyVector(X + other.X, Y + other.Y);
+            return new Vector(X + other.X, Y + other.Y);
         }
 
-        public ReadOnlyVector WithX(double x)
+        public Vector WithX(double x)
         {
-            return new ReadOnlyVector(x, Y);
+            return new Vector(x, Y);
         }
 
-        public ReadOnlyVector WithY(double y)
+        public Vector WithY(double y)
         {
-            return new ReadOnlyVector(X, y);
+            return new Vector(X, y);
+        }
+    }
+
+    public static class Geometry
+    {
+        public static double GetLength(Vector vector)
+        {
+            return Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+        }
+
+        public static Vector Add(Vector firstVector, Vector secondVector)
+        {
+            return new Vector(firstVector.X + secondVector.X,
+                              firstVector.Y + secondVector.Y);
+        }
+
+        public static double GetLength(Segment segment)
+        {
+            return Math.Sqrt(
+                (segment.End.X - segment.Begin.X) * (segment.End.X - segment.Begin.X)
+                + (segment.End.Y - segment.Begin.Y) * (segment.End.Y - segment.Begin.Y));
+        }
+
+        public static bool IsVectorInSegment(Vector vector, Segment segment)
+        {
+            return Math.Abs(Math.Sqrt(Math.Pow(vector.X - segment.Begin.X, 2)
+                + Math.Pow(vector.Y - segment.Begin.Y, 2))
+                + Math.Sqrt(Math.Pow(segment.End.X - vector.X, 2)
+                + Math.Pow(segment.End.Y - vector.Y, 2))
+                - GetLength(segment)) < double.Epsilon;
         }
     }
 }
